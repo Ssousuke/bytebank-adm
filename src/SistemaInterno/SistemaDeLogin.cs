28dcabbcf7d0af015c9e9bf69ae8e8278465f3ce
Wesley Farias <wesleysousukee@gmail.com>
2022-07-10 20:31:06 -0300

    renomeia diretorio

---
+++ src/SistemaInterno/SistemaDeLogin.cs
@@ -0,0 +1,12 @@
+﻿using bytebank_adm.Funcionarios;
+
+namespace bytebank_adm.SistemaInterno;
+
+public class SistemaInterno
+{
+    public bool Logar(Diretor funcionario, string login, string senha)
+    {
+        bool usuarioAutenticado = funcionario.Autenticacao(login, senha);
+        return usuarioAutenticado;
+    }
+}
